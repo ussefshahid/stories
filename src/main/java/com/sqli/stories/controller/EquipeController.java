61abda22b04a1dfdc45d91f1792465e230f9de6e
younes hamdane <younes.hamdane11@gmail.com>
2019-03-07 17:20:31 +0100

    update equipe controller (deleteEquipe method )

--- src/main/java/com/sqli/stories/controller/EquipeController.java
+++ src/main/java/com/sqli/stories/controller/EquipeController.java
@@ -3,6 +3,7 @@ package com.sqli.stories.controller;
 import com.sqli.stories.entities.Equipe;
 import com.sqli.stories.services.EquipeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,17 +11,19 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/app")
+@CrossOrigin(origins = "http://localhost:3000")
 public class EquipeController {
 
     @Autowired
     private EquipeService equipeService;
 
     @PostMapping("/equipe")
-    public ResponseEntity<Equipe> addEquipe(@RequestBody  Equipe equipe) {
+    public ResponseEntity<Equipe> addEquipe(@RequestBody Equipe equipe) {
         return ResponseEntity.ok(equipeService.addEquipe(equipe));
     }
+
     @PutMapping("/equipe")
-    public ResponseEntity<Equipe> updateEquipe(@RequestBody  Equipe equipe) {
+    public ResponseEntity<Equipe> updateEquipe(@RequestBody Equipe equipe) {
         return ResponseEntity.ok(equipeService.addEquipe(equipe));
     }
 
@@ -28,12 +31,16 @@ public class EquipeController {
     public ResponseEntity<List<Equipe>> getAllEquipes() {
         return ResponseEntity.ok(equipeService.getAllEquipes());
     }
+
     @DeleteMapping("/equipe/{id}")
-    public void deleteEquipe(@PathVariable("id") long idEquipe) {
-        equipeService.deleteEquipe(idEquipe);
+    public ResponseEntity<Void> deleteEquipe(@PathVariable(value = "id", required = true) long idEquipe) {
+        try {
+            equipeService.deleteEquipe(idEquipe);
+            return ResponseEntity.noContent().build();
+        } catch (ResourceNotFoundException ex) {
+            return ResponseEntity.notFound().build();
+        }
     }
-
-
 
 
 }
